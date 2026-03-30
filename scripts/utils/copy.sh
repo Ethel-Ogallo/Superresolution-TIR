9b0c8bef14f5032e6377943352e0cf0cb2868742
Ethel-Ogallo <ogalloethel@gmail.com>
2026-03-30 12:24:43 +0200

    preprocessing TIR data

---
+++ scripts/utils/copy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+RSYNC="rsync"
+
+SOURCE="/home/ogallo/Documents/CDE/MSC_thesis/Superresolution-TIR/TIR+LS_test/processed/patches"
+
+DEST="e2406751@dmis:/share/home/e2406751/sisr/"
+
+while true; do
+  $RSYNC -av --progress --partial --append-verify --timeout=180 \
+    -e "ssh -o ServerAliveInterval=10 -o ServerAliveCountMax=30 -o TCPKeepAlive=yes -o IPQoS=throughput" \
+    "$SOURCE" "$DEST"
+
+  if [ $? -eq 0 ]; then
+    echo "Transfer completed successfully."
+    exit 0
+  fi
+
+  echo "Connection dropped — sleeping 10 seconds before retry..."
+  sleep 10
+done
